b7e173b0078426fd91a202350342048ef116b882
turneor <webdohod781@gmail.com>
2018-01-16 22:16:35 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+var api = 'http://52.87.163.163:32888';
+var blockTargetInterval = 120;
+var coinUnits = 100000000;
+var symbol = 'YTR';
+var refreshDelay = 30000;
+// pools stats by MainCoins
+var networkStat = {
+    "ytr": [
+        ["your pool name", "http://your pool name"],
+
+
+    ]
+};
