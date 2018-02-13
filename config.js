5e13b2db3fe1fb170463288f6c0bcf8e2a400518
crypto <webdohod781@gmail.com>
2018-02-13 17:43:34 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var api = 'http://52.87.163.163:32888';
-var blockTargetInterval = 120;
-var coinUnits = 100000000;
+var blockTargetInterval = 240;
+var coinUnits = 18000000;
 var symbol = 'YTR';
 var refreshDelay = 30000;
 // pools stats by MainCoins
